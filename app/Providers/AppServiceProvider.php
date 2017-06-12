a7499fa27f8de0034d834624f53d19e347dc18d3
Daniel Berry <daniel@shanedaniels.com>
2017-06-12 13:10:09 -0500

    adds packages that help with development. * Laravel IDE Helper - https://github.com/barryvdh/laravel-ide-helper * Laravel Debugbar - https://github.com/barryvdh/laravel-debugbar * Packager - https://github.com/Jeroen-G/Laravel-Packager * Artisan View Manager - https://github.com/svenluijten/artisan-view
    * Decomposer - https://github.com/lubusIN/laravel-decomposer
    
    These packages are loaded in the AppServiceProvider when we are in local or testing environments.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if ($this->app->environment() !== 'production') {
+            /* https://github.com/barryvdh/laravel-ide-helper */
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+
+            /* https://github.com/Jeroen-G/Laravel-Packager */
+            $this->app->register(\JeroenG\Packager\PackagerServiceProvider::class);
+
+            /* https://github.com/svenluijten/artisan-view */
+            $this->app->register(\Sven\ArtisanView\ArtisanViewServiceProvider::class);
+
+            /* https://github.com/barryvdh/laravel-debugbar */
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+            $this->app->alias('Debugbar', \Barryvdh\Debugbar\Facade::class);
+
+            /* https://github.com/lubusIN/laravel-decomposer */
+            $this->app->register(\Lubusin\Decomposer\DecomposerServiceProvider::class);
+        }
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+if (app()->environment('local', 'testing')) {
+    Route::get('decompose','\Lubusin\Decomposer\Controllers\DecomposerController@index');
+}
